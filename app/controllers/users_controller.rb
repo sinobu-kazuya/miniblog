e15601ab0b8581affe43f863fc272956125587d9
信夫知也 <kazooie.nobuo@gmail.com>
2018-08-16 23:56:55 +0900

    Done paginate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
    user = User.find(params[:id])
-   @blogs =user.blogs.order("created_at DESC")
+   @blogs =user.blogs.page(params[:page]).per(4).order("created_at DESC")
   end
 end
